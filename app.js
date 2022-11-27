e5e5851875dd14e64d53020a7c17906e05baa894
Kyle Groening <kyle.groening@cytiva.com>
2022-11-26 20:04:59 -0500

    Add fill-in square when hovered over

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 // Create reference to container div
 const container = document.querySelector(".container");
-// Populate container div with 16 x 16 grid of square divs
-    // Create new div
 const numberOfRows = 16;
 const numberOfColumns = 16;
+
+// Populate container div with 16 x 16 grid of square divs
 for (let rowindex = 0; rowindex < numberOfRows; rowindex++) {
     const row = document.createElement('div');
     row.classList.add(`row`);
-    console.log('row div created')
     for (let colindex = 0; colindex < numberOfColumns; colindex++) {
         const column = document.createElement('div');
         column.classList.add(`column`)
         row.appendChild(column);
-        console.log('column div created')
     }
     container.appendChild(row);
 }
+
+// Add event listener to each div in the grid to look for hover and change style to filled
+gridDiv = document.querySelectorAll('.column');
+gridDiv.forEach(element => {
+    element.addEventListener("mouseover",(e)=>{
+        e.target.classList.add("filled");
+    })
+});
+
