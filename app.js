5df4d2866f5a80a9d5b658fd44f35b96fabf2e4f
Kyle Groening <kyle.groening@cytiva.com>
2022-11-27 06:35:26 -0500

    Add change resolution function

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 // Create reference to container div
 const container = document.querySelector(".container");
-let numberOfRows = 16;
 let numberOfColumns = 16;
+generateGrid();
+
+// Change resolution based on user input
+const resolutionBtn = document.getElementById('change-resolution');
+resolutionBtn.addEventListener('click', e => {
+    numberOfColumns = prompt("Enter number of pixels per row.", 16)
+    if (numberOfColumns == null || numberOfColumns == "") {
+        numberOfColumns = 16;
+    }
+    removeAllChildNodes(container);
+    generateGrid();
+})
 
 // Populate container div with 16 x 16 grid of square divs
-for (let rowindex = 0; rowindex < numberOfRows; rowindex++) {
-    const row = document.createElement('div');
-    row.classList.add(`row`);
-    for (let colindex = 0; colindex < numberOfColumns; colindex++) {
-        const column = document.createElement('div');
-        column.classList.add(`column`)
-        row.appendChild(column);
+function generateGrid() {
+    for (let rowindex = 0; rowindex < numberOfColumns; rowindex++) {
+        const row = document.createElement('div');
+        row.classList.add(`row`);
+        for (let colindex = 0; colindex < numberOfColumns; colindex++) {
+            const column = document.createElement('div');
+            column.classList.add(`column`)
+            row.appendChild(column);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    addFillEvent();
 }
 
 
 // Add event listener to each div in the grid to look for hover and change style to filled
-gridDiv = document.querySelectorAll('.column');
-gridDiv.forEach(element => {
-    element.addEventListener("mouseover",(e)=>{
-        e.target.classList.add("filled");
-    })
-});
+function addFillEvent() {
+    gridDiv = document.querySelectorAll('.column');
+    gridDiv.forEach(element => {
+        element.addEventListener("mouseover", (e) => {
+            e.target.classList.add("filled");
+        });
+    });
+}
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
